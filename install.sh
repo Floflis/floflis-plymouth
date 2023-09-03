49508ea5df7f4e38b6b3193668dba3aec4ba264f
Daniella Mesquita <mesquitadaniella@proton.me>
2023-09-03 20:56:53 -0300

    install.sh: remove the testing feature

--- install.sh
+++ install.sh
@@ -9,10 +9,10 @@ clear
 sudo update-alternatives --install /usr/share/plymouth/themes/default.plymouth default.plymouth /usr/share/plymouth/themes/floflis-plymouth/floflis-plymouth.plymouth 100
 sudo update-alternatives --config default.plymouth  #here, choose the number of the theme you want to use then hit enter
 sudo update-initramfs -u
-echo
-echo Installing plymouth-x11...   Redundant if already installed.
-sudo apt-get install plymouth-x11
-echo
-echo Running 10-second test...
-sudo plymouthd ; sudo plymouth --show-splash ; for ((I=0; I<10; I++)); do sleep 1 ; sudo plymouth --update=test$I ; done ; sudo plymouth --quit
+#echo
+#echo Installing plymouth-x11...   Redundant if already installed.
+#sudo apt-get install plymouth-x11
+#echo
+#echo Running 10-second test...
+#sudo plymouthd ; sudo plymouth --show-splash ; for ((I=0; I<10; I++)); do sleep 1 ; sudo plymouth --update=test$I ; done ; sudo plymouth --quit
 exit
